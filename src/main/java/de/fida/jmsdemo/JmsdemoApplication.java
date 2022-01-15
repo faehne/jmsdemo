6db85b61a10e1710a67072868becb198eb204f73
faehne <h.faehnrich@fida.de>
2022-01-15 20:25:13 +0100

    Varianten

--- src/main/java/de/fida/jmsdemo/JmsdemoApplication.java
+++ src/main/java/de/fida/jmsdemo/JmsdemoApplication.java
@@ -26,60 +26,85 @@ public class JmsdemoApplication {
 	public ConnectionFactory connectionFactory(){
 		ActiveMQConnectionFactory activeMQConnectionFactory  = new ActiveMQConnectionFactory();
 		activeMQConnectionFactory.setBrokerURL("tcp://localhost:61616?jms.redeliveryPolicy.maximumRedeliveries=5");
+		activeMQConnectionFactory.setUseAsyncSend(true); //true --> wesentlich schneller aber auch nicht mehr save
+		//Embedded Version starten
 		//activeMQConnectionFactory.setBrokerURL("vm://embedded??broker.persistent=false?jms.redeliveryPolicy.maximumRedeliveries=5&jms.useAsyncSend=true");
 		return  activeMQConnectionFactory;
 	}
 	@Bean
-	public DefaultJmsListenerContainerFactory myFactory(){
+	public DefaultJmsListenerContainerFactory myFactory(ConnectionFactory connectionFactory){
 		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-		factory.setConnectionFactory(connectionFactory());
-		//factory.setSessionTransacted(true);
+		factory.setConnectionFactory(connectionFactory);
+
+		////////////////////////// 1. Consume per Transaction ////////////////////////
+//		factory.setSessionTransacted(true);
+//		factory.setSessionAcknowledgeMode(Session.SESSION_TRANSACTED);
+
+		////////////////////////// 2. Consume per AutoAck - Nachricht wird nicht wiederholt; kein DLQ-Handling ////////////////////////
 		factory.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
 		return factory;
 	}
 
 	public static void main(String[] args) throws JMSException {
 		ConfigurableApplicationContext ctx = SpringApplication.run(JmsdemoApplication.class, args);
+
+		//////////////// 1. Mit lokaler Sessiontransanction  über JmsTemplate ///////////////////////////
 		JmsTemplate jmsTemplate = ctx.getBean(JmsTemplate.class);
 		JmsTransactionManager transactionManager = new JmsTransactionManager(jmsTemplate.getConnectionFactory());
 		TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
 		jmsTemplate.setExplicitQosEnabled(true);
-		jmsTemplate.setDeliveryPersistent(false);
+		jmsTemplate.setDeliveryPersistent(true); //Kein DLQ-Handling, wenn false
 		transactionTemplate.executeWithoutResult(transactionStatus -> {
-			for(int i = 0; i < 50000;i++) {
-				jmsTemplate.send("test2", session -> {
+			for(int i = 0; i < 10000;i++) {
+				jmsTemplate.send("test", session -> {
 					TextMessage msg = session.createTextMessage("Hallo Welt");
-					//msg.setJMSDeliveryMode(DeliveryMode.NON_PERSISTENT);
 					return msg;
 				});
 			}
 		});
 
-//		throw new RuntimeException();
+		//////////////// 2. Mit AutoAck  über JmsTemplate und non-persistent ///////////////////////////
+//		JmsTemplate jmsTemplate = ctx.getBean(JmsTemplate.class);
+//
+//		jmsTemplate.setExplicitQosEnabled(true);
+//		jmsTemplate.setDeliveryPersistent(false); //Kein DLQ-Handling, wenn false
+//		for(int i = 0; i < 50000;i++) {
+//			jmsTemplate.send("test", session -> {
+//				TextMessage msg = session.createTextMessage("Hallo Welt");
+//				return msg;
+//			});
+//		}
 
+		//////////////// 3. Mit lokaler Sessiontransanction  über plain jms ///////////////////////////
+//		ConnectionFactory connectionFactory = ctx.getBean(ConnectionFactory.class);
+//		Connection conn = connectionFactory.createConnection();
+//		Session session = conn.createSession(true, Session.SESSION_TRANSACTED);
+//		Destination destination = session.createQueue("test");
+//		MessageProducer producer = session.createProducer(destination);
+//
+//		for(int i = 0; i < 50000;i++) {
+//			TextMessage msg = session.createTextMessage("Hallo Welt");
+//			msg.setJMSDeliveryMode(DeliveryMode.PERSISTENT); //Kein DLQ-Handling, wenn NON_PERSISTENT
+//			producer.send(msg);
+//		}
+//		session.commit();
+//		session.close();
+//		conn.close();
+
+		//////////////// 4. AutoAck über plain jms ///////////////////////////
 //		ConnectionFactory connectionFactory = ctx.getBean(ConnectionFactory.class);
 //		Connection conn = connectionFactory.createConnection();
 //		Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//		Destination destination = session.createQueue("test1");
+//		Destination destination = session.createQueue("test");
 //		MessageProducer producer = session.createProducer(destination);
 //
-//		for(int i = 0; i < 100000;i++) {
+//		for(int i = 0; i < 1000000;i++) {
 //			TextMessage msg = session.createTextMessage("Hallo Welt");
-//			msg.setJMSDeliveryMode(DeliveryMode.NON_PERSISTENT);
+//			msg.setJMSDeliveryMode(DeliveryMode.NON_PERSISTENT); //Kein DLQ-Handling, wenn NON_PERSISTENT
 //			producer.send(msg);
 //		}
-//		System.out.println("before commit");
-//		//session.commit();
-//		System.out.println("after commit");
 //		session.close();
 //		conn.close();
-//		for(int i = 0; i < 100000;i++)
-//			jmsTemplate.send("test2", session -> {
-//				TextMessage msg = session.createTextMessage("Hallo Welt");
-//				//msg.setJMSDeliveryMode(DeliveryMode.NON_PERSISTENT);
-//				return msg;
-//			});
-//		throw new RuntimeException();
 
 	}
 
--- src/main/java/de/fida/jmsdemo/Listener.java
+++ src/main/java/de/fida/jmsdemo/Listener.java
@@ -15,15 +15,16 @@ public class Listener  implements MessageListener {
     private double avg = 1000;
     private double sum = 0;
 
-    @JmsListener(destination = "test2", containerFactory = "myFactory")
+    @JmsListener(destination = "test", containerFactory = "myFactory")
     public void onMessage(Message message) {
         try {
             cnt++;
             sum = sum + (System.currentTimeMillis() - message.getJMSTimestamp());
             avg = sum / cnt;
             if(cnt % 1000 == 0) {
-                System.out.println("In-Cnt: " + cnt + "... avg-msg-time: " + avg);
+                System.out.println("In-Cnt: " + cnt + "... avg-msg-time (ms): " + avg);
             }
+            // Zum Testen: Rollback auslösen - geht nicht bei Autoack
             //throw new RuntimeException("--------------------ROLLBACK--------------------------");
         } catch (JMSException e) {
             e.printStackTrace();
