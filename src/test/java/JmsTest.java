9eb1aa115391a74c0cc1a315363cc4cf189af135
faehnrichh <h.faehnrich@fida.de>
2023-04-03 03:42:15 +0200

    test

--- src/test/java/JmsTest.java
+++ src/test/java/JmsTest.java
@@ -1,11 +1,14 @@
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.junit.EmbeddedActiveMQBroker;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
@@ -18,22 +21,50 @@ public class JmsTest {
     @Rule
     public EmbeddedActiveMQBroker broker = new EmbeddedActiveMQBroker();
 
+    private Connection connection;
+    private Queue queue;
+    private Session session;
+    private MessageProducer producer;
+
+    private MessageConsumer consumer;
+    @Before
+    public void before() throws JMSException {
+        ConnectionFactory factory = new ActiveMQConnectionFactory("vm://embedded-broker?create=false");
+        connection = factory.createConnection();
+        connection.start();
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        queue = session.createQueue("test");
+        producer = session.createProducer(queue);
+        consumer = session.createConsumer(queue);
+    }
+
+    @After
+    public void after() throws JMSException {
+        producer.close();
+        consumer.close();
+        session.close();
+        connection.close();
+    }
     @Test
     public void test() throws Exception {
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://embedded-broker?create=false");
-        final Queue queue;
-        final Connection connection = connectionFactory.createConnection();
-        connection.start();
-        final Session session = connection.createSession(false,
-                Session.AUTO_ACKNOWLEDGE);
-        queue = session.createQueue("test");
-        final MessageProducer producer = session.createProducer(queue);
         TextMessage message = session.createTextMessage("testing");
         producer.send(message);
-        MessageConsumer consumer = session.createConsumer(queue);
         message = (TextMessage) consumer.receive();
         Assert.assertEquals("testing", message.getText());
 
+    }
+
+    @Test
+    public void testMulti() throws Exception {
+        int cntExpected = 200;
+        for (int i = 0; i < cntExpected; i++) {
+            TextMessage message = session.createTextMessage("testing" + i);
+            producer.send(message);
+        }
+        for (int i = 0; i < cntExpected; i++) {
+            TextMessage message = (TextMessage) consumer.receive();
+            Assert.assertEquals("testing" + i, message.getText());
+        }
     }
 
 }
